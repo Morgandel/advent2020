c2977db6d7a64b5c0cb0e12841625581373312e4
Morgandel <axel.khampet@live.fr>
2020-12-01 14:21:20 +0100

    day1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -5,20 +5,52 @@ def readFile(pName):
     content = [int(line.strip('\n')) for line in f]
     return content
 
-def getInputSum(pContent, pSum):
-    k = 0
-    for i in range(len(pContent)):
+def getInputSumOfTwo(pContent, pSum):
+    m = 1
+    found = False
+    for i in range(len(pContent)-1):
         inp1 = pContent[i]
-        for j in range(k, len(pContent)):
+        for j in range(m, len(pContent)):
             inp2 = pContent[j]
             if(inp1 + inp2 == pSum):
                 print(inp1, inp2, inp1*inp2)
-        k+=1
+                found = True
+                break;
+        if found:
+            break;
+        m+=1
+
+def getInputSumOfThree(pContent, pSum):
+    m=0
+    n=1
+    found = False
+    for i in range(len(pContent)-2):
+        m+=1
+        n+=1
+        o=m
+        p=n
+        inp1 = pContent[i]
+        for j in range(o, len(pContent)):
+            inp2 = pContent[j]
+            for k in range(p, len(pContent)):
+                inp3 = pContent[k]
+                if(inp1 + inp2 + inp3 == pSum):
+                    print(inp1, inp2, inp3, inp1*inp2*inp3)
+                    found = True
+                    break;
+            if found:
+                break;
+        if found:
+            break;
 
 if __name__ == "__main__":
-    if (len(sys.argv) == 2):
-        listInput = readFile(sys.argv[1])
-        getInputSum(listInput, 2020)
+    if (len(sys.argv) == 3):
+        listInput = readFile(sys.argv[2])
+        if(sys.argv[1] == "-tw"):
+            getInputSumOfTwo(listInput, 2020)
+        elif(sys.argv[1] == "-th"):
+            getInputSumOfThree(listInput, 2020)
+
     else:
         print("Please give the name of the imput file")
 
